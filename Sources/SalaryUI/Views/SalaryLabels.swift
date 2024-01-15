731c0131692ba5cc6eaab60421a622a580b1d20f
Rodrigo Torres <b.rodrigotorres@gmail.com>
2024-01-14 22:38:30 -0400

    Add new labels

--- Sources/SalaryUI/Views/SalaryLabels.swift
+++ Sources/SalaryUI/Views/SalaryLabels.swift
@@ -7,14 +7,101 @@
 
 import SwiftUI
 
-struct SalaryLabels: View {
+struct InstructionsText: View {
+    let text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(text.uppercased())
+            .bold()
+            .multilineTextAlignment(.center)
+            .lineSpacing(4.0)
+            .font(.footnote)
+            .kerning(2.0)
+            .padding(.horizontal, 30)
+    }
+}
+
+struct BigNumberText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .kerning(-1.0)
+            .font(.largeTitle)
+            .fontWeight(.black)
+    }
+}
+
+struct LabelText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text.uppercased())
+            .kerning(1.5)
+            .font(.caption)
+            .bold()
+    }
+}
+
+struct BodyText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .multilineTextAlignment(.center)
+            .lineSpacing(12)
+    }
+}
+
+struct ButtonText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+            .padding()
+            .font(.headline)
+            .fontWeight(.bold)
+            .frame(maxWidth: .infinity)
+            .background(
+                Color.accentColor
+            )
+            .foregroundColor(.white)
+            .cornerRadius(12)
+    }
+}
+
+struct SalaryText: View {
+    let score: Int
+    
+    var body: some View {
+        Text(String(score))
+            .bold()
+            .kerning(-0.2)
+            .font(.title3)
+    }
+}
+
+struct TextViewPreview: View {
+    var body: some View {
+        VStack {
+            BigNumberText(text: "000")
+            InstructionsText(text: "Instructions")
+            LabelText(text: "Score")
+            BodyText(text: "You scored 200 points\n 😻")
+            ButtonText(text: "Start New Round")
+            SalaryText(score: 200)
+        }
+        .padding()
+        .previewDevice("Iphone 14")
     }
 }
 
 struct SwiftUIView_Previews: PreviewProvider {
     static var previews: some View {
-        SalaryLabels()
+        VStack {
+            TextViewPreview()
+        }
     }
 }
