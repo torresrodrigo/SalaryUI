27195375e198ce47ea77326c6e42d56066df8f11
Rodrigo Torres <b.rodrigotorres@gmail.com>
2024-01-14 22:02:57 -0400

    First Commit

---
+++ Sources/SalaryUI/Views/SalaryLabels.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Rodrigo on 14/01/2024.
+//
+
+import SwiftUI
+
+struct SalaryLabels: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SalaryLabels()
+    }
+}
